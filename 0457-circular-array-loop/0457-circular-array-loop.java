17e6b7380c99c159834c5c61b66ad1f80babaef2
sanchiitaaa <sanchita092006@gmail.com>
2026-09-21 23:32:25 +0530

    Time: 0 ms (100%), Space: 42.2 MB (99.01%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -1,43 +1,67 @@
 class Solution {
-    int nextIndex(int index, int[] nums)
-    {
-        int n = nums.length;
-        return ((index + nums[index]) % n + n)% n;
+
+    public int calcIndex(int nums[], int curr) {
+        return ((curr + nums[curr]) % nums.length + nums.length) % nums.length;
     }
+
     public boolean circularArrayLoop(int[] nums) {
         int n = nums.length;
 
         for (int i = 0; i < n; i++) {
-            boolean direction = nums[i] > 0;
 
+            if (nums[i] == 0) {
+                continue;
+            }
+
+            boolean isPos = nums[i] > 0;
             int slow = i;
             int fast = i;
 
             while (true) {
-                int nextSlow = nextIndex(slow, nums);
 
-                if (nextSlow == slow || (nums[nextSlow] > 0) != direction) {
-                    break;
-                }
+                int nextSlow = calcIndex(nums, slow);
 
-                int nextFast = nextIndex(fast, nums);
-
-                if (nextFast == fast || (nums[nextFast] > 0) != direction) {
-                    break;
-                }
-
-                int nextFast2 = nextIndex(nextFast, nums);
-
-                if (nextFast2 == nextFast || (nums[nextFast2] > 0) != direction) {
+                if (nums[nextSlow] == 0 ||
+                    (nums[nextSlow] > 0) != isPos ||
+                    nextSlow == slow) {
                     break;
                 }
 
                 slow = nextSlow;
-                fast = nextFast2;
+
+                int nextFast = calcIndex(nums, fast);
+
+                if (nums[nextFast] == 0 ||
+                    (nums[nextFast] > 0) != isPos ||
+                    nextFast == fast) {
+                    break;
+                }
+
+                fast = nextFast;
+
+                nextFast = calcIndex(nums, fast);
+
+                if (nums[nextFast] == 0 ||
+                    (nums[nextFast] > 0) != isPos ||
+                    nextFast == fast) {
+                    break;
+                }
+
+                fast = nextFast;
 
                 if (slow == fast) {
                     return true;
                 }
+            }
+
+            int curr = i;
+
+            while (nums[curr] != 0 &&
+                   (nums[curr] > 0) == isPos) {
+
+                int next = calcIndex(nums, curr);
+                nums[curr] = 0;
+                curr = next;
             }
         }
 
